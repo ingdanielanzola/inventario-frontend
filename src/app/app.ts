fe300ee0c033882f52e9aa8553026ca9a9de561f
Daniel Anzola <Anzola_1402@hotmail.com>
2026-05-11 20:31:04 -0500

    primer commit - frontend Angular inventario

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ProductosComponent } from './components/productos/productos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [ProductosComponent],
+  template: '<app-productos></app-productos>'
 })
-export class App {
-  protected readonly title = signal('inventario-frontend');
-}
+export class App {}
---
+++ src/app/components/productos/productos.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-productos',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './productos.html',
+  styleUrl: './productos.css'
+})
+export class ProductosComponent implements OnInit {
+
+  productos: any[] = [];
+  nuevoProducto = { nombre: '', categoria: '', precio: 0, stock: 0 };
+  editando: boolean = false;
+  productoEditando: any = { nombre: '', categoria: '', precio: 0, stock: 0 }; 
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.cargar();
+  }
+
+  cargar(): void {
+  this.http.get<any[]>('http://localhost:8080/api/productos').subscribe(data => {
+    this.productos = data;
+    this.cd.detectChanges();
+  });
+}
+
+  agregar(): void {
+    this.http.post('http://localhost:8080/api/productos', this.nuevoProducto).subscribe(() => {
+      this.cargar();
+      this.nuevoProducto = { nombre: '', categoria: '', precio: 0, stock: 0 };
+    });
+  }
+  editar(producto: any): void {
+    this.editando = true;
+    this.productoEditando = { ...producto };
+  }
+
+  guardar(): void {
+    this.http.put(`http://localhost:8080/api/productos/${this.productoEditando.id}`, this.productoEditando).subscribe(() => {
+      this.cargar();
+      this.editando = false;
+    });
+  }
+
+  cancelar(): void {
+    this.editando = false;
+  }
+  eliminar(id: number): void {
+    this.http.delete(`http://localhost:8080/api/productos/${id}`).subscribe(() => {
+      this.cargar();
+    });
+  }
+}
